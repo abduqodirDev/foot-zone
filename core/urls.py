e7b916dfac140ad544ffa82d05f17731d7dd78b7
abduqodir <abduqodirdusmurodov@gmail.com>
2024-11-16 13:40:47 +0300

    fixed

--- core/urls.py
+++ core/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('stadion/', include('stadion.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
-    path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
