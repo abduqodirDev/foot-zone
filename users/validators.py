d12ce1d64aaf3266a9340d1545e9bf835003dca0
abduqodir <abduqodirdusmurodov@gmail.com>
2025-01-08 08:27:43 +0300

    fix: users

--- users/validators.py
+++ users/validators.py
@@ -4,18 +4,10 @@ from django.core.exceptions import ValidationError
 
 
 def check_phone_validator(obj):
-    if obj[0] == '+':
-        if str(obj)[1:].isdigit:
-            if obj.startswith('+998') and len(str(obj))==13:
-                return True
-            elif obj.startswith('+') and len(str(obj))==10:
-                return True
-            else:
-                raise ValidationError(message="phone number is not valid")
-        else:
-            raise ValidationError(message="phone number is not valid")
-    else:
+    if not obj.startswith('+998') or len(obj) != 13 or not obj[1:].isdigit():
         raise ValidationError(message="phone number is not valid")
+    else:
+        return True
 
 
 def check_code_validator(obj):
