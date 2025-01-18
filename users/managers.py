3634ed302123c253b2c245df5105739c599bd089
abduqodir <abduqodirdusmurodov@gmail.com>
2025-01-18 07:40:14 +0300

    fix

--- users/managers.py
+++ users/managers.py
@@ -5,24 +5,21 @@ from django.apps import apps
 
 
 class CustomManager(BaseUserManager):
-    def _create_user(self, username, password, **extra_fields):
-        if not username:
-            raise ValueError("The given username must be set")
+    def _create_user(self, password, **extra_fields):
         GlobalUserModel = apps.get_model(
             self.model._meta.app_label, self.model._meta.object_name
         )
-        username = GlobalUserModel.normalize_username(username)
-        user = self.model(username=username, **extra_fields)
+        user = self.model(**extra_fields)
         user.password = make_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, password=None, **extra_fields):
+    def create_user(self, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", False)
         extra_fields.setdefault("is_superuser", False)
-        return self._create_user(username, password, **extra_fields)
+        return self._create_user(password, **extra_fields)
 
-    def create_superuser(self, username, password=None, **extra_fields):
+    def create_superuser(self, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("role", "SUPERUSER")
@@ -32,4 +29,4 @@ class CustomManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(username, password, **extra_fields)
+        return self._create_user(password, **extra_fields)
